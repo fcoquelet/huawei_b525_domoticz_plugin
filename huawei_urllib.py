d1b28e50b875e34fadecf38c14c3fd7f69f6a830
Florent Coquelet <18007124+fcoquelet@users.noreply.github.com>
2017-10-04 00:43:56 +0200

    Moving to python3

--- huawei_urllib.py
+++ huawei_urllib.py
@@ -1,12 +1,11 @@
 # coding: utf-8
-from __future__ import unicode_literals
-import vcr
+#from __future__ import unicode_literals
 import hashlib
 import base64
 import sys
-import collections,urllib2, xml.etree.ElementTree as ET
+import collections,urllib.request as request, xml.etree.ElementTree as ET
 
-SALTED_PASSWD = u'***REMOVED***'
+SALTED_PASSWD = '***REMOVED***'
 
 class ClientMetaData(dict):
     COOKIE_LABEL = "Cookie"
@@ -23,13 +22,13 @@ class ClientMetaData(dict):
         if ClientMetaData.COOKIE_LABEL in self:
             self.pop(ClientMetaData.COOKIE_LABEL)
 
-    def refreshSessionCookieIfNeeded(self, urllib2Info):
-       new_cookie=urllib2Info.getheader("Set-"+ClientMetaData.COOKIE_LABEL)
+    def refreshSessionCookieIfNeeded(self, requestInfo):
+       new_cookie=requestInfo.get("Set-"+ClientMetaData.COOKIE_LABEL)
        if new_cookie:
            self.setSessionCookie(new_cookie)
 
-    def refreshVerificationTokenIfNeeded(self, urllib2Info):
-        new_token=urllib2Info.getheader(ClientMetaData.VERIF_TOKEN_LABEL)
+    def refreshVerificationTokenIfNeeded(self, requestInfo):
+        new_token=requestInfo.get(ClientMetaData.VERIF_TOKEN_LABEL)
         if new_token:
             self.setVerificationToken(new_token)
 
@@ -68,8 +67,8 @@ class Client:
         try:
                 if data and not self.metadata.hasVerificationToken():
                     self.refreshToken()
-                req = urllib2.Request(url='http://'+self.MODEM_IP+self.API_PATH+api_call, headers=self.metadata)
-                xmlobj = urllib2.urlopen(req,data)
+                req = request.Request(url='http://'+self.MODEM_IP+self.API_PATH+api_call, headers=self.metadata)
+                xmlobj = request.urlopen(req,data)
         except:
                 print('[!] Error while making GET request for', api_call)
                 exit(1)
@@ -86,7 +85,6 @@ class Client:
     def getToken(self):
         self.headers = {}
         resp = self.sendReceive("webserver/SesTokInfo")
-        print resp
         self.metadata.setSessionCookie(resp.find("SesInfo").text)
         self.metadata.setVerificationToken(resp.find("TokInfo").text)
 #api/webserver/SesTokInfo
@@ -105,11 +103,12 @@ class Client:
         req = ET.Element('request')
         ET.SubElement(req,'Username').text = user
         ET.SubElement(req,'password_type').text = '4'
-        ET.SubElement(req,'Password').text=base64.b64encode(hashlib.sha256(user+SALTED_PASSWD+self.metadata.getVerificationToken()).hexdigest())
+        ET.SubElement(req,'Password').text=base64.b64encode(hashlib.sha256((user+SALTED_PASSWD+self.metadata.getVerificationToken()).encode('utf-8')).hexdigest().encode('utf-8')).decode()
         
         root = self.sendReceive('user/login',ET.tostring(req,encoding='UTF-8', method='xml'))
         self.metadata.clearVerificationToken()
-        return self.isLogged()
+        ET.dump(root)
+        return root.text == 'OK' 
 
     def isLogged(self):
        root = self.sendReceive('user/state-login')
@@ -139,31 +138,42 @@ def get_usage(client):
 
         
 def send_sms(client, tels, text):
-    if client.isLogged(): 
-        req = ET.Element('request')
-        ET.SubElement(req,'Index').text = '-1'
-        phones=ET.SubElement(req,'Phones')
-        for tel in tels:
-            ET.SubElement(phones,'Phone').text=tel
-        ET.SubElement(req,'Content').text = text
-        ET.SubElement(req,'Length').text = str(len(text))
-        ET.SubElement(req,'Reserved').text = "1"
-        ET.SubElement(req,'Date').text = "2017-10-01 23:01:30"
-        root = client.sendReceive('sms/send-sms',ET.tostring(req, encoding="UTF-8", method="xml"))
-    else:
+    if not client.isLogged():
         raise NotLoggedException('Need to be logged to send SMS')
+    req = ET.Element('request')
+    ET.SubElement(req,'Index').text = '-1'
+    phones=ET.SubElement(req,'Phones')
+    for tel in tels:
+        ET.SubElement(phones,'Phone').text=tel
+    ET.SubElement(req,'Content').text = text
+    ET.SubElement(req,'Length').text = str(len(text))
+    ET.SubElement(req,'Reserved').text = "1"
+    ET.SubElement(req,'Date').text = "2017-10-01 23:01:30"
+    root = client.sendReceive('sms/send-sms',ET.tostring(req, encoding="UTF-8", method="xml"))
+    return root.text == "OK"
 
+def is_data_enabled(client):
+    root = client.sendReceive('dialup/mobile-dataswitch')
+    return int(root.find('dataswitch').text)==1
+
+def enable_data(client,enable=True):
+    if not client.isLogged():
+        raise NotLoggedException('Need to be logged to switch data')
+    req = ET.Element('request')
+    ET.SubElement(req,'dataswitch').text = str(int(enable))
+    root = client.sendReceive('dialup/mobile-dataswitch',ET.tostring(req, encoding="UTF-8", method="xml"))
+    return root.text == "OK"
 
 # return signal strength percentage
 def get_signal(client):
         root = client.sendReceive('monitoring/status')
         return int(root.find('SignalIcon').text)
 
-with vcr.use_cassette('fixtures/vcr_cassettes/huawei.yaml'):
-    huawei_client = Client("***REMOVED***.252")
-    huawei_client.getToken()
-    huawei_client.login()
-    print  huawei_client.isLogged()
-    send_sms(huawei_client,["***REMOVED***","***REMOVED***"],"Il y a {} barres de signal et nous avons consomme {}/100 du forfait !".format(get_signal(huawei_client),get_usage(huawei_client)))
-    print('DATA PLAN USAGE: {}%'.format(get_signal(huawei_client)))
+huawei_client = Client("***REMOVED***.252")
+huawei_client.getToken()
+print(huawei_client.login())
+print(huawei_client.isLogged())
+print(is_data_enabled(huawei_client))
+enable_data(huawei_client,True)
+#send_sms(huawei_client,["***REMOVED***"],"Il y a {} barres de signal et nous avons consomme {}/100 du forfait !".format(get_signal(huawei_client),get_usage(huawei_client)))
 
